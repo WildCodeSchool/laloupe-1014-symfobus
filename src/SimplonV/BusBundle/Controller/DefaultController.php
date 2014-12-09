8dd2e4f757c513b9b4b52341a5a7c2f72a404d85
romaincoeur <coeurro@gmail.com>
2014-12-09 15:39:35 +0100

    Create connection form

--- src/SimplonV/BusBundle/Controller/DefaultController.php
+++ src/SimplonV/BusBundle/Controller/DefaultController.php
@@ -2,12 +2,50 @@
 
 namespace SimplonV\BusBundle\Controller;
 
+use SimplonV\BusBundle\Entity\User;
+use SimplonV\BusBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('SimplonVBusBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = new User();
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find User entity.');
+        }
+
+        $form = $this->createForm(new UserType(), $entity);
+
+        return $this->render('SimplonVBusBundle:Default:index.html.twig', array(
+            'entity'      => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    public function connexionAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $entity = $em->getRepository('SimplonVBusBundle:User')->findOneByEmail($request->get("email"));
+        $form = $this->createForm(new UserType(), $entity);
+
+        if ($form->isValid()) {
+            if ($entity->getPassword() == $request->get("password"))
+            return $this->redirect($this->generateUrl('connexionOK'));
+        }
+
+        return $this->render('SimplonVBusBundle:Default:index.html.twig', array(
+            'entity'      => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    public function connexionOkAction()
+    {
+        return $this->render('SimplonVBusBundle:Default:connexionOK.html.twig');
     }
 }
