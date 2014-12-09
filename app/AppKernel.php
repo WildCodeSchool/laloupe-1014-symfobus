0dc9f406c7ba6f5b0820ab51792d7bcf7f0f8e2b
diabolika <maxence@pantaleon.tk>
2014-12-09 17:47:51 +0100

    route admins

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,14 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new SimplonV\BusBundle\SimplonVBusBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\CacheBundle\SonataCacheBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle()
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
